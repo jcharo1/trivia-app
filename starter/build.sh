baab3c5b1ff6c03d28cdde273e8ccdcc3d470626
Justin <justinrcharo@gmail.com>
2022-05-31 20:43:29 -0500

    docker

--- starter/build.sh
+++ starter/build.sh
@@ -2,3 +2,11 @@ docker build \
     -t jcharo/trivia_app_backend \
     --build-arg PASS=$PASS \
     .
+
+
+
+
+# docker run --name backend_trivia_v2 -dp 5000:5000 trivia_app_backend
+
+
+# docker run -it --rm -v ${PWD}:/app -v /app/node_modules -p 3000:3000 frontend_test
