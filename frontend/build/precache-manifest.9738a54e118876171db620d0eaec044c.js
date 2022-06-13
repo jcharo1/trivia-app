8bf6a2a172f8510985617a49877a193a0caa35c6
Justin <justinrcharo@gmail.com>
2022-06-13 03:27:42 -0500

    config

---
+++ frontend/build/precache-manifest.9738a54e118876171db620d0eaec044c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4123f3fe614fba658a384c4dbb685a63",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a34981d946e477168fdd",
+    "url": "/static/css/main.8c945f4a.chunk.css"
+  },
+  {
+    "revision": "072f3e95e3a0453af94c",
+    "url": "/static/js/2.6d5d0db4.chunk.js"
+  },
+  {
+    "revision": "a34981d946e477168fdd",
+    "url": "/static/js/main.8fbe1cb0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
